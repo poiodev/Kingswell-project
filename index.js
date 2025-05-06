e576203ef61fec8d3e124b29c2178d9f696228b9
Valentina Maldonado <poiowtf@MacBook-Air-de-Valentina.local>
2025-05-06 15:19:33 -0500

    update type module

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
-app.use(express.static('public')); // Carpeta con tu index.html
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
